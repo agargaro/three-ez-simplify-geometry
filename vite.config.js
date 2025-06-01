4e430de60ea904849304cf0d3be64095f1c89cfb
Andrea Gargaro <devgargaro@gmail.com>
2025-06-01 12:19:46 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { externalizeDeps } from 'vite-plugin-externalize-deps';
+
+export default defineConfig(({ command }) => ({
+  publicDir: command === 'build' ? false : 'public',
+  resolve: {
+    alias: {
+      '@three.ez/package-name': resolve(__dirname, 'src/index.ts')
+    }
+  },
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'build/index',
+      formats: ['es', 'cjs']
+    }
+  },
+  plugins: [
+    externalizeDeps(),
+    viteStaticCopy({
+      targets: [{
+        src: ['LICENSE', 'package.json', 'README.md'],
+        dest: './'
+      }]
+    })
+  ]
+}));
